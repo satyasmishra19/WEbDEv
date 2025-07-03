610357751a4749c4e8ae27bee6b727151fcaf2ea
Satyashree Mishra <143271875+satyasmishra19@users.noreply.github.com>
2025-07-04 01:50:26 +0530

    Faulty calculator method by harry

--- Faulty calculator/Faulty_calculator.js
+++ Faulty calculator/Faulty_calculator.js
@@ -2,6 +2,7 @@
 npm install prompt-sync
 so if at start of this file it show the error then copy paste the above and run at terminal and then run the program
 */
+/*
 const prompt = require('prompt-sync')();
 
 const No1 = parseInt(prompt('Please enter your 1st number: '));
@@ -34,4 +35,31 @@ if (Math.random()<0.1) {
     if (operator == "/") {
         console.log(No1/No2);
     }
+}
+*/
+
+
+let random = Math.random()
+let a = prompt("Enter first number")
+let c = prompt("Enter operation")
+let b = prompt("Enter second number")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+console.log(random)
+if (random > 0.1) {
+    //perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+
+else{
+    //perform wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
 }
